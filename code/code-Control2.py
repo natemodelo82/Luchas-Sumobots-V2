67be8d664968a22da935b636d25d4109e45c9756
natemodelo82 <rfallas@gmail.com>
2025-06-15 00:42:05 -0600

    Add files via upload

---
+++ code/code-Control2.py
@@ -0,0 +1,105 @@
+##########################################################################################################
+#                                                                                                        #
+#                                                                                                        #
+#                                                                                                        #
+#                                              _____                                                     #
+#                                             /  \  @@  \                                                #
+#                                            /       \ %# @  \                                           #
+#                                           /    (_)    \ % @   \                                        #
+#                                          /         (_)   \ _____                                       #
+#                                         /                /      |                                      #
+#                                         \               /       |                                      #
+#                                           \            /     __ |                                      #
+#                                              \        /     /  |                                       #
+#                                                 \    /-  - -\__/                                       #
+#                                                                                                        #
+#                               ___                       __         ___________                         #
+#                             / ___|| | | |  \/  |/ _ \ | __ )  / _ \__   __/ ___|                       #
+#                             \___ \| | | | |\/| | | | ||  _ \ | | | | | |  \___ \                       #
+#                              ___) | |_| | |  | | |_| || |_) || |_| | | |   ___) |                      #
+#                             |____/ \___/|_|  |_|\___/ |____/  \__ /  | |  |____/                       #                                                #
+#                                                                                                        #
+#                                                                                                        #
+#            Control remoto para   SUMOBOTS de lucha                                                     #
+#                                                                                                        #
+#                   Colegio Técnico Profesional de Oreamuno, Informática Empresarial                     #
+#                                                                                                        #
+#                               Integrantes:                                                             #
+#                                                                                                        #
+#                                  - Montenegro Araya Manfred                                            #
+#                                  - Mora Torres Jazmín                                                  #
+#                                  - Ramírez Guzmán Celeste                                              #
+#                                  - Vega Camacho María José                                             #
+#                                                                                                        #  
+#                               Profesor Tutor:                                                          #
+#                                    Ronald Fallas Rojas                                                 #
+#                                                                                                        #
+#                                        Año: 2025                                                       #
+#                                      Versión: 1.0                                                      #
+##########################################################################################################
+
+
+from ideaboard import IdeaBoard
+from time import sleep
+import board
+import sys
+import espnow #libreria para comunicarse con otro IdeaBoard
+import keypad
+
+mac_x = b'\x08:\x8d\x8e3\xc0'
+mac_z = b'\x80do\x110<'
+
+
+mac_a = b'\x08:\x8d\x8e4h'
+mac_b = b'\x80do\x10\xf6\xc0'
+sumo1 = mac_z
+
+
+tiempoEspera = 0.1
+tiempoInicio = 1
+
+ib = IdeaBoard()
+e = espnow.ESPNow()
+sumobot = espnow.Peer(mac=sumo1)
+e.peers.append(sumobot)
+
+#botones            Golpe          derecha     abajo      izquierda     arriba
+keys = keypad.Keys((board.IO27, board.IO33, board.IO32, board.IO25, board.IO26,), value_when_pressed=False, pull=True)
+
+#
+#Envia un mensaje al otro Ideaboard usando el protocolo espnow
+def enviarMensajeSumobot(mensaje):
+    e.send(mensaje)
+
+sleep(tiempoInicio)
+
+while True:
+    
+    event = keys.events.get()
+    if event:
+        if event.pressed:
+            if (event.key_number == 4):
+                print("Arriba")
+                enviarMensajeSumobot("arriba")
+            if (event.key_number == 2):
+                print("Abajo")
+                enviarMensajeSumobot("abajo")
+            if (event.key_number == 3):
+                print("Izquierda")
+                enviarMensajeSumobot("izquierda")
+            if (event.key_number == 1):
+                print("Derecha")
+                enviarMensajeSumobot("derecha")
+            if (event.key_number == 0):
+                print("Rojo")
+                enviarMensajeSumobot("rojo")
+                
+            print(event)
+
+    sleep(tiempoEspera)
+    
+
+
+
+
+
